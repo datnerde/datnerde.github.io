9c244a772010b8e56c6dfba4322f7a941fad2122
mzhong45 <146140351+mzhong45@users.noreply.github.com>
2025-08-21 15:55:03 +0000

    Deploying to gh-pages from @ datnerde/datnerde.github.io@4595418d78904f2c59122f8643d2e2576296ee98 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -115,6 +115,13 @@ ninja.data = [{
           window.open("https://medium.com/@ming.zhong", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=BseYF6MAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
